cc2e9f9c28a604cd7508b6a7e9c2beaa4a887e69
Daniel Seong <Daniel.Seong@cudirect.com>
2020-02-06 16:58:10 -0800

    specified db employee table id to be non null identity value

--- MVCDemoDB/dbo/Tables/Employee.sql
+++ MVCDemoDB/dbo/Tables/Employee.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Employee]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [EmployeeId] INT NOT NULL, 
     [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL, 
